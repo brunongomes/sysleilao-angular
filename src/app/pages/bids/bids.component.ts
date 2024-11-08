41db964d8fc382fce33f479478e04530a9b1c1ae
Bruno Gomes <brunongomes8@gmail.com>
2024-11-08 14:45:37 -0300

    feat: create bidding component

---
+++ src/app/pages/bids/bids.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../../shared/menu/menu.component';
+
+@Component({
+  selector: 'app-bids',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './bids.component.html',
+  styleUrl: './bids.component.css'
+})
+export class BidsComponent {
+
+}
