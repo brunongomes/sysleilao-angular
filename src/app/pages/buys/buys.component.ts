b453f450f66f0c3b49eb048f2e8087a19e210698
Bruno Gomes <brunongomes8@gmail.com>
2024-11-08 14:45:54 -0300

    feat: create purchasing component

---
+++ src/app/pages/buys/buys.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../../shared/menu/menu.component';
+
+@Component({
+  selector: 'app-buys',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './buys.component.html',
+  styleUrl: './buys.component.css'
+})
+export class BuysComponent {
+
+}
