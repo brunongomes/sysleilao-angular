21a5c1d4a6963715dc908da1508e166d7debce4a
Bruno Gomes <brunongomes8@gmail.com>
2024-11-08 14:44:54 -0300

    feat: create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/login/home/home.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { BidsComponent } from './pages/bids/bids.component';
+import { BuysComponent } from './pages/buys/buys.component';
+import { ServiceComponent } from './pages/service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'bids', component: BidsComponent},
+    {path: 'buys', component: BuysComponent},
+    {path: 'service', component: ServiceComponent},
+];
