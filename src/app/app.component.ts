ce62cb4dcd7ceb0b24f37b9c218e3340125cecdb
Bruno Gomes <brunongomes8@gmail.com>
2024-11-08 14:44:29 -0300

    feat: import routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { HomeComponent } from './pages/login/home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { BidsComponent } from './pages/bids/bids.component';
+import { BuysComponent } from './pages/buys/buys.component';
+import { ServiceComponent } from './pages/service/service.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    MenuComponent, 
+    ProgressComponent,
+    BidsComponent,
+    BuysComponent,
+    ServiceComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'leilao';
+  constructor(private router: Router) {}
+
+  isLoginPage(): boolean {
+    return this.router.url === '/home';
+  }
 }
